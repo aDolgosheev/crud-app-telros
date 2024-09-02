c5a12d27b88aa89fa2400a79b85b0a3ad95dcec6
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2024-09-03 00:11:09 +0300

    2024.09.03 initial commit

---
+++ src/main/java/ru/dolgosheev/crudapptelros/CrudAppTelrosApplication.java
@@ -0,0 +1,13 @@
+package ru.dolgosheev.crudapptelros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAppTelrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudAppTelrosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/dolgosheev/crudapptelros/CrudAppTelrosApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dolgosheev.crudapptelros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppTelrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
